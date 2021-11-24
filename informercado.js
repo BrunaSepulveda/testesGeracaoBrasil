86c0ee3130a5ed8ebc4e065f48c6cd22524f9ad0
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-11-24 13:49:14 -0300

    infomercado

--- informercado.js
+++ informercado.js
@@ -13,13 +13,13 @@ const cheerio = require('cheerio')
     const planilha = await download(`${link}`);
     let planilhaObj = xlsx.parse(planilha).find((element) => element.name === '002 Usinas').data;
     planilhaObj.splice(0,14)
-    const ultimaPosicao = planilhaObj.length - 11
-    planilhaObj.splice(ultimaPosicao,10)
+    const ultimaPosicao = planilhaObj.length - 13932
+    planilhaObj.splice(ultimaPosicao,13931)
     let informercados = []
     planilhaObj.forEach((linha) => {
-      if (linha && linha.length > 0) {
+      if (linha[3] && linha[3] !== '') {
         const objLinhasTabela = {ceg: '', potencia:'', garatiaFisica: '',descontoTUST:'', participaDoMRE:''}
-        objLinhasTabela.ceg = !linha[3] && linha[3] === '' ? null : linha[3]
+        objLinhasTabela.ceg = linha[3]
         objLinhasTabela.potencia = !linha[16] && linha[16] ==='' ? null : linha[16]
         objLinhasTabela.garatiaFisica = !linha[17] && linha[17] ==='' ? null : linha[17]
         objLinhasTabela.descontoTUST = !linha[15] && linha[15] ==='' ? null : linha[15]
@@ -27,7 +27,6 @@ const cheerio = require('cheerio')
         informercados.push(objLinhasTabela)
       }
     })
-    console.log({primeiro:informercados[0], ultimo: informercados[informercados.length - 1]})
   } catch (err) {
     console.error(err)
   }
