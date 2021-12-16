9a5176dcbc3c1a750f666770000726973de07f10
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-12-16 18:39:10 -0300

    continuaçao

--- consultaCeg.js
+++ consultaCeg.js
@@ -1,10 +1,11 @@
 const axios = require("axios").default;
 const cheerio = require("cheerio");
+const { data } = require('./dadosCeg');
 
 async function consultaCeg() {
-  const apiPrimeiraPag = await axios.get(
-    "https://www2.aneel.gov.br/scg/consulta_empreendimento.asp?acao=BUSCAR&pagina=1&IdTipoGeracao=&IdFaseUsina=&CodCIE=&NomeEmpreendimento="
-  );
+  // const apiPrimeiraPag = await axios.get(
+  //   "https://www2.aneel.gov.br/scg/consulta_empreendimento.asp?acao=BUSCAR&pagina=1&IdTipoGeracao=&IdFaseUsina=&CodCIE=&NomeEmpreendimento="
+  // );
 
   // const $ = cheerio.load(apiPrimeiraPag.data);
   // let quantidade = $('td.linhaBranca:contains("Quantidade")').text();
@@ -21,43 +22,55 @@ async function consultaCeg() {
   //   iteravel.push(index);
   // }
 
-  function primeiraPagObjLinhasTabela(response) {
-    return extrairDados(response);
-  }
-
-  function extrairDados(response) {
-    const $ = cheerio.load(response.data);
-    const table = $(".tabelaMaior")[2];
-    const linhas = [];
-
-    $(table)
-      .find('tr')
-      .each((_i, linha) => {
-        const linhaPag = []
-        $(linha)
-          .find('td')
-          .each((_index, colunaPorLinha) => {
-        linhaPag.push($(colunaPorLinha).text().trim())
-      })
-      linhas.push(linhaPag)
-    })
-
-    return linhas
-  }
-
- const linhasPrimeiraPag = primeiraPagObjLinhasTabela(apiPrimeiraPag);
- console.log(linhasPrimeiraPag[1], linhasPrimeiraPag[2], linhasPrimeiraPag[3])
-  // const tabelaCompleta = [];
-  // for await (let pag of iteravel) {
-  //   if (pag === 1) {
-  //     tabelaCompleta.push(...corpoPrimeiraPag);
-  //   } else {
-  //     const corpoPorPag = await objLinhasTabela(pag);
-  //     tabelaCompleta.push(...corpoPorPag);
-  //   }
+  // function primeiraPagObjLinhasTabela(response) {
+  //   return extrairDados(response);
   // }
-  // return tabelaCompleta;
+
+  // function extrairDados(response) {
+  //   const $ = cheerio.load(response.data);
+  //   const table = $(".tabelaMaior")[2];
+  //   const linhas = [];
+
+  //   $(table)
+  //     .find('tr')
+  //     .each((_i, linha) => {
+  //       const linhaPag = []
+  //       $(linha)
+  //         .find('td')
+  //         .each((_index, colunaPorLinha) => {
+  //       linhaPag.push($(colunaPorLinha).text().trim())
+  //     })
+  //     linhas.push(linhaPag)
+  //   })
+
+  //   return linhas
+  // }
+  function emptyValueToNull(value) {
+    return !value || value === ' - ' ? null : value;
+  }
+  function regexCegResumido(ceg) {
+    return ceg.match(/\d{6}-\d{1}/g) ? ceg.match(/\d{6}-\d{1}/g)[0] : null;
+  }
+
+  function formatterLinhas(linhasPorPagina) {
+    const corpoPorPag = [];
+    linhasPorPagina.forEach((linha) => {
+      const cegResumido = regexCegResumido(linha[0])
+      // tipo linha[1] nomeEmprendimento linha[2]
+    });
+  }
 }
 
 consultaCeg()
 module.exports = consultaCeg;
+
+ceg
+tipo
+nomeEmprendimento
+potenciaMw
+garantiaFisicaMw
+razaoSocial
+cnpj
+municipio
+uf
+dataOperacao
